eb6b2fd17dbb6434bd7464795f37d3aa2371545c
01020021 <arthur.luziano@araujo.com.br>
2026-08-18 15:23:00 -0300

    TextHelper (swapLastTwoCharacters) - Minha resolução antes de assistir à aula.

--- src/main/java/com/b/simple/design/business/text/TextHelper.java
+++ src/main/java/com/b/simple/design/business/text/TextHelper.java
@@ -3,7 +3,16 @@ package com.b.simple.design.business.text;
 public class TextHelper {
 
 	public String swapLastTwoCharacters(String str) {
-		return null;
+		if (str.isBlank() || str.length() < 2) return str;
+
+		int lastIndex = str.length() - 1;
+		int secondLastIndex = str.length() - 2;
+		char lastChar = str.charAt(lastIndex);
+		char secondLastChar = str.charAt(secondLastIndex);
+
+		str = str.substring(0, secondLastIndex);
+
+		return str + lastChar + secondLastChar;
 	}
 
 	public String truncateAInFirst2Positions(String str) {
